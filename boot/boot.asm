1c97e48d6335a4623f8fd8957d8102175753b049
Trishvan <dabbirutrisvhan@gmail.com>
2024-10-19 23:20:31 +0530

    more fixes

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,18 +1,78 @@
-BITS 64
-org 0x7c00
+; Bootloader - Switch to Long Mode
+bits 16
+[org 0x7c00]
 
 start:
-    ; Switch to 64-bit mode
-    ; Setup the stack and call the kernel
-    ; More setup code can go here
+    cli                       ; Disable interrupts
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7c00            ; Set up stack
 
-    ; Jump to the kernel
-    jmp kernel_main
+    ; Enable A20 line (necessary for protected mode)
+    in al, 0x92
+    or al, 00000010b
+    out 0x92, al
 
-kernel_main:
-    ; Your kernel's main code goes here
-    ; For example, initializing the screen or CLI
-    jmp $
+    ; Load GDT
+    lgdt [gdt_descriptor]
 
-times 510 - ($ - $$) db 0
-dw 0xaa55
+    ; Enter Protected Mode
+    mov eax, cr0
+    or eax, 1                 ; Set the PE (Protection Enable) bit
+    mov cr0, eax
+
+    jmp CODE_SEG:init_pm       ; Far jump to 32-bit code segment
+
+[bits 32]
+init_pm:
+    ; Set up segment registers for protected mode
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+
+    ; Switch to Long Mode
+    mov ecx, 0xC0000080        ; Read Extended Feature Enable Register (EFER)
+    rdmsr
+    or eax, 0x00000100         ; Set Long Mode Enable bit
+    wrmsr
+
+    mov eax, cr4
+    or eax, 0x20               ; Enable PAE (Physical Address Extension)
+    mov cr4, eax
+
+    ; Enable Long Mode
+    mov eax, cr0
+    or eax, 0x80000001         ; Enable Paging and Protection Mode
+    mov cr0, eax
+
+    ; Load 64-bit kernel
+    jmp 0x28:kernel_start      ; Jump to Long Mode code at CODE_SEG descriptor
+
+[bits 64]
+kernel_start:
+    ; 64-bit kernel starts here
+    hlt                        ; Hang the system
+
+; GDT definition
+align 8
+gdt_start:
+    dq 0                       ; Null descriptor
+    dq 0x00AF9A000000FFFF       ; Code Segment descriptor (64-bit code segment)
+    dq 0x00AF92000000FFFF       ; Data Segment descriptor (64-bit data segment)
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1  ; GDT size
+    dd gdt_start                ; GDT base address
+gdt_end:
+
+; Define segment selectors
+CODE_SEG equ 0x08              ; Code segment offset in GDT
+DATA_SEG equ 0x10              ; Data segment offset in GDT
+
+times 510 - ($ - $$) db 0       ; Pad to 512 bytes
+dw 0xAA55                       ; Boot signature
--- test/build.sh
+++ test/build.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 
-# Create the build directory if it doesn't exist
-if [ ! -d "../build" ]; then
-  mkdir ../build
-fi
+# Variables
+BOOT_IMG="../build/os_image.img"
+PADDING_IMG="../build/padded.img"
+ISO_FILE="../build/os_image.iso"
+BOOT_BIN="../boot/boot.bin"  # Bootloader binary
+KERNEL_BIN="../build/kernel.bin"  # Kernel binary to be appended
 
-# Padding the os_image.img file to 1.44MB
-dd if=/dev/zero bs=512 count=2880 of=../build/os_image.img
-dd if=../build/os_image.img bs=512 conv=notrunc seek=0 count=2880
+# Padding size for a 1.44MB image
+PADDING_SIZE=$((1440 * 1024))  # 1.44MB in bytes
 
-# Move boot binary and kernel binary into os_image.img
-cat ../boot/boot.bin ../build/kernel.bin > ../build/os_image.img
+# Create a padded image (empty sectors filled with zeros to match 1.44MB)
+echo "Padding the boot image to 1.44MB..."
+dd if=/dev/zero bs=1 count=$PADDING_SIZE of="$PADDING_IMG"
 
-# Create an ISO file
-xorriso -as mkisofs -b os_image.img -no-emul-boot -boot-load-size 4 -boot-info-table -o ../build/os_image.iso ../build/os_image.img
+# Concatenate boot.bin and kernel.bin into the final padded image
+cat "$BOOT_BIN" "$KERNEL_BIN" > "$BOOT_IMG"
 
-if [ $? -eq 0 ]; then
-  echo "ISO successfully created."
-else
-  echo "Error creating ISO."
-  exit 1
-fi
+# Pad the final image to 1.44MB
+dd if="$BOOT_IMG" of="$PADDING_IMG" conv=notrunc bs=1 count=$PADDING_SIZE
+
+# Replace the boot image with the padded one
+mv "$PADDING_IMG" "$BOOT_IMG"
+
+# Create the ISO file
+echo "Creating ISO file..."
+xorriso -as mkisofs -r -J -b os_image.img -no-emul-boot -boot-load-size 4 -boot-info-table -o "$ISO_FILE" ../build/
+
+# Clean up
+echo "Cleaning up..."
+rm "$PADDING_IMG"
+
+echo "Done! The ISO file is: $ISO_FILE"
